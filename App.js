b9c62965d86e91e0fd2b585a7be512b474c8dfd6
Cecilia Giunta <93303178+cecigiunta@users.noreply.github.com>
2022-05-27 21:59:07 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import TaskListComponent from './components/containers/taskList';
+// import LoginForm from './components/pure/forms/loginForm';
+import Registro from './components/pure/forms/registerForm';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskListComponent></TaskListComponent>
+      
+
+    </div>
+  );
+}
+
+export default App;
